299e9e7a0581066bbb5660e1e14652c28d747335
Mateusz Matysek <mateusziom1@gmail.com>
2025-04-03 15:21:05 +0200

    Add 'get href' from links in script.js

--- js/script.js
+++ js/script.js
@@ -2,22 +2,13 @@
 
 //List of links
 
-
-const clickLinkHandler = function() {
-    const clickedElement = this;
-    clickedElement.classList.add('active');
-    const articleName = clickedElement.getAttribute('href');
-    console.log('articleName:', articleName);
-}
-
 const titleClickHandler = function(event){
-    console.log('Link was clicked!');
-    console.log('event:', event);
+    /* [DONE] prevent default action for clicked link */
+    event.preventDefault();
     /*[IN PROGRESS] Variables for easy access */
     const clickedElement = this;
     const activeLinks = document.querySelectorAll('.titles a.active');
     const activeArticles = document.querySelectorAll('.post.active');
-
     /* [DONE] remove class 'active' from all article links  */
     for(let activeLink of activeLinks){
       activeLink.classList.remove('active');
@@ -29,8 +20,9 @@ const titleClickHandler = function(event){
     for(let activeArticle of activeArticles){
         activeArticle.classList.remove('active');
     }
-    /* [IN PROGRESS] get 'href' attribute from the clicked link */
-    
+    /* [DONE] get 'href' attribute from the clicked link */
+    const articleName = clickedElement.getAttribute('href');
+    console.log('articleName:', articleName);
     /* [IN PROGRESS] find the correct article using the selector (value of 'href' attribute) */
   
     /* [IN PROGRESS] add class 'active' to the correct article */
