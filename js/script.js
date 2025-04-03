0519e1c2fe42a568c63e900fec4f549c30dc3c92
Mateusz Matysek <mateusziom1@gmail.com>
2025-04-03 15:07:30 +0200

    Add 'remove active' for links and articles in in script.js

--- js/script.js
+++ js/script.js
@@ -2,29 +2,32 @@
 
 //List of links
 
-// const links = document.querySelectorAll('.titles a');
-// console.log('links:', links);
 
-// const clickLinkHandler = function() {
-//     const clickedElement = this;
-//     clickedElement.classList.add('active');
-//     const articleName = clickedElement.getAttribute('href');
-//     console.log('articleName:', articleName);
-// }
+const clickLinkHandler = function() {
+    const clickedElement = this;
+    clickedElement.classList.add('active');
+    const articleName = clickedElement.getAttribute('href');
+    console.log('articleName:', articleName);
+}
 
-// for(const link of links) {
-
-//     link.addEventListener('click', clickLinkHandler)
-// }
 const titleClickHandler = function(event){
     console.log('Link was clicked!');
     console.log('event:', event);
     /* remove class 'active' from all article links  */
-  
+    const activeLinks = document.querySelectorAll('.titles a.active');
+
+    for(let activeLink of activeLinks){
+      activeLink.classList.remove('active');
+    }
     /* add class 'active' to the clicked link */
-  
+    const clickedElement = this;
+    clickedElement.classList.add('active');
     /* remove class 'active' from all articles */
-  
+    const activeArticles = document.querySelectorAll('.post.active');
+
+    for(let activeArticle of activeArticles){
+        activeArticle.classList.remove('active');
+    }
     /* get 'href' attribute from the clicked link */
   
     /* find the correct article using the selector (value of 'href' attribute) */
