9af74599b2c69205893f2e8578aec8f09d86ee46
Mateusz Matysek <mateusziom1@gmail.com>
2025-04-03 16:04:47 +0200

    Add 'find the correct' in script.js

--- js/script.js
+++ js/script.js
@@ -5,27 +5,38 @@
 const titleClickHandler = function(event){
     /* [DONE] prevent default action for clicked link */
     event.preventDefault();
-    /*[IN PROGRESS] Variables for easy access */
-    const clickedElement = this;
-    const activeLinks = document.querySelectorAll('.titles a.active');
-    const activeArticles = document.querySelectorAll('.post.active');
     /* [DONE] remove class 'active' from all article links  */
+    const activeLinks = document.querySelectorAll('.titles a.active');
+
     for(let activeLink of activeLinks){
       activeLink.classList.remove('active');
     }
     /* [DONE] add class 'active' to the clicked link */
+    const clickedElement = this;
     console.log('clickedElement:', clickedElement);
     clickedElement.classList.add('active');
     /* [DONE] remove class 'active' from all articles */
+    const activeArticles = document.querySelectorAll('.post.active');
+
     for(let activeArticle of activeArticles){
         activeArticle.classList.remove('active');
     }
     /* [DONE] get 'href' attribute from the clicked link */
-    const articleName = clickedElement.getAttribute('href');
-    console.log('articleName:', articleName);
-    /* [IN PROGRESS] find the correct article using the selector (value of 'href' attribute) */
-  
+    const articleHref = clickedElement.getAttribute('href');
+    console.log('articleName:', articleHref);
+    /* [DONE] find the correct article using the selector (value of 'href' attribute) */
+    const listArticles = document.querySelectorAll('.post');
+    console.log('list of articles:', listArticles);
+    let correctArticle = 0;
+
+    for(let article of listArticles){
+        const idArticle = article.getAttribute('id');
+        if(articleHref === '#'+idArticle){
+            correctArticle = idArticle;
+        }
+    }
     /* [IN PROGRESS] add class 'active' to the correct article */
+    console.log(correctArticle);
   }
   
   const links = document.querySelectorAll('.titles a');
